78e7ae3b622c398bd49bb31bbe5120fb84071ca4
Trevor Redfern <trevor@paytrace.com>
2012-11-07 15:53:51 -0800

    created a little example for running the request

--- examples/add_lead.rb
+++ examples/add_lead.rb
@@ -1,7 +1,15 @@
 require 'zoro'
 
+Zoro.auth_token = 'some-valid-auth-token'
+Zoro.http_connection = Faraday::Connection.new do |conn|
+  conn.request :url_encoded
+  conn.response :logger #Log Faraday to STDOUT for easier testing
+  conn.adapter Faraday.default_adapter
+end
+
 lead = Zoro::Lead.new
-lead.first_name = "Test"
-lead.last_name = "trevor trevor"
-lead.email = "trevor@paytrace.com"
-lead.save!
+lead.add_field "First Name", "Test"
+lead.add_field "Last Name", "trevor trevor"
+lead.add_field "Email", "trevor@paytrace.com"
+response = lead.save!
+puts response.body
