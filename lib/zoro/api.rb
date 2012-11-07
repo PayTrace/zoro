d5a11e2978d3ec6424ac621918205953ffa4a5b2
Trevor Redfern <trevor@paytrace.com>
2012-11-07 10:07:56 -0800

    Cleaned up url generation

--- lib/zoro/api.rb
+++ lib/zoro/api.rb
@@ -3,13 +3,13 @@ require 'faraday'
 module Zoro
   class Api
     def format_url(mod, method)
-      "https://crm.zoho.com/crm/private/xml/#{mod}/#{method}?newFormat=1&authtoken=#{Zoro.auth_token}&scope=crmapi"
+      "https://crm.zoho.com/crm/private/xml/#{mod}/#{method}"
     end
 
     def insert_records(record)
       url = format_url(record.zoho_module, "insertRecords")
 
-      Zoro.http_connection.post(url, record)
+      Zoro.http_connection.post(url, record, :newformat => 1, :authtoken => Zoro.auth_token, :scope => 'crmapi')
     end
   end
 end
--- test/zoro/api_test.rb
+++ test/zoro/api_test.rb
@@ -6,14 +6,14 @@ describe Zoro::Api do
     Zoro.auth_token = "1234"
     api = Zoro::Api.new
     url = api.format_url("Leads", "insertRecords")
-    url.must_equal "https://crm.zoho.com/crm/private/xml/Leads/insertRecords?newFormat=1&authtoken=1234&scope=crmapi"
+    url.must_equal "https://crm.zoho.com/crm/private/xml/Leads/insertRecords"
   end
 
   it "can insert records into the zoro CRM" do
     #Stub HTTP
     Zoro.auth_token = "1234"
     stubs = stub_http
-    stubs.post("/crm/private/xml/Leads/insertRecords?newFormat=1&authtoken=1234&scope=crmapi") {}
+    stubs.post("/crm/private/xml/Leads/insertRecords") {[200, {}, 'foo']}
 
     record = stub(:zoho_module => "Leads")
     api = Zoro::Api.new
