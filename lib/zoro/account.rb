aa09e58b6f0a83da19bcae4d77d51fafc6601e44
john <john@paytrace.com>
2019-12-10 11:50:17 -0800

    MM-123: fix the account boarding fields need underscore

--- lib/zoro/account.rb
+++ lib/zoro/account.rb
@@ -1,5 +1,13 @@
 module Zoro
   class Account < Record
+    def initialize(data = {})
+      @fields = {}.merge(data)
+      @zoho_module = "Accounts"
+    end
+
+    def add_field(field_name, value)
+      @fields[field_name] = value
+    end
   end
 end
 
