782c739da74914fd1b63598566f25b4636c2b2e0
john <john@paytrace.com>
2019-12-10 12:47:48 -0800

    MM-123: the field method sucks

--- lib/zoro/field_name.rb
+++ lib/zoro/field_name.rb
@@ -2,12 +2,12 @@ module Zoro
   module FieldName
     def self.make_field(name)
       # clean_up = name.gsub(/_/, ' ')
-      clean_up = clean_up.gsub(/=/, '')
+      clean_up = name.gsub(/=/, '')
 
       words = clean_up.split 
-      words.map do |w|
-        w.capitalize
-      end.join(" ")
+      # words.map do |w|
+      #   w.capitalize
+      # end.join(" ")
     end
   end
 end
